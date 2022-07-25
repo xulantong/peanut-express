7454a9cacde8cb7a26709bb8c0dd9a3c303a4f85
xulantong <1457501818@qq.com>
2022-07-25 15:31:16 +0800

    first commit

--- api/information.js
+++ api/information.js
@@ -21,12 +21,42 @@ app.post("/getTree", (request, response) => {
         } else {
             let originData = JSON.parse(data)
             return response.send({
-                    code: 200,
-                    msg: "请求成功",
-                    success:true,
-                    result: originData
+                code: 200,
+                msg: "请求成功",
+                success: true,
+                result: originData
             })
         }
+    })
+})
+app.get("/getInfo", (request, response) => {
+    response.setHeader("Access-Control-Allow_origin", '*')
+    response.setHeader("cache-Control", "no-store, no-cache, max-age=0")
+    fs.readFile("./data/info/db.json", (err, data) => {
+        if (err) {
+            return console.log("读取失败！");
+        } else {
+            let originData = JSON.parse(data)
+            return response.send({
+                code: 200,
+                msg: "请求成功",
+                success: true,
+                result: originData
+            })
+        }
+    })
+})
+app.post("/saveInfo", (request, response) => {
+    response.setHeader("Access-Control-Allow_origin", '*')
+    response.setHeader("cache-Control", "no-store, no-cache, max-age=0")
+    fs.writeFile("./data/info/db.json", JSON.stringify(request.body), (err) => {
+        return console.log("读取失败！");
+    })
+    return response.send({
+        code: 200,
+        msg: "保存成功",
+        success: true,
+        result: null
     })
 })
 
